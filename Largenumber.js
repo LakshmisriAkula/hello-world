f11323f11afeaa4c22f3f083b9e780cc0ad06621
Lakshmisri Akula <akulalakshmisri@gmail.com>
2023-04-20 16:58:15 +0530

    changes

---
+++ Largenumber.js
@@ -0,0 +1,16 @@
+function maxnum(num1,num2){
+ 
+    if(num1>num2)
+        return num1;
+    
+    else
+       return num2;
+    
+    }
+    
+    
+    function main(){
+      var num1 = 5;
+      var num2 = 10;
+        console.log(maxnum(num1,num2));
+    }
