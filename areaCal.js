0452887dcab4167dd8dcf9aff77c3e6657be83d5
Lakshmisri Akula <akulalakshmisri@gmail.com>
2023-04-26 13:04:43 +0530

    changes

--- areaCal.js
+++ areaCal.js
@@ -5,26 +5,46 @@ const shape = prompt('Enter which area to calculate ( either Square, Rectangle, 
 switch (shape) {
     case 'Square':
         const side = parseFloat(prompt('Enter Side: '));
-        area = side * side;
-        console.log(area);
+
+        if (side < 0) {
+            console.log('You need positive integers to calculate area!');
+        }
+        else {
+            area = side * side;
+            console.log(area);
+        }
+
+
         break;
     case 'Rectangle':
         const length = parseFloat(prompt('Enter Length: '));
         const width = parseFloat(prompt('Enter Width: '));
-        area = length * width;
-        console.log(area);
+        if (length < 0 || width < 0) {
+            console.log('You need positive integers to calculate area!');
+        } else {
+            area = length * width;
+            console.log(area);
+        }
         break;
     case 'Triangle':
         const base = parseFloat(prompt('Enter Base: '));
         const height = parseFloat(prompt('Enter Height: '));
+        if (base < 0 || height < 0) {
+            console.log('You need positive integers to calculate area!');
+        } else {
         area = base * height / 2;
         console.log(area);
+        }
         break;
     case 'Circle':
         const radius = parseFloat(prompt('Enter Radius: '));
-
+        if (radius < 0) {
+            console.log('You need positive integers to calculate area!');
+        }
+        else {
         area = Math.PI * radius * radius;
         console.log(area);
+        }
         break;
 
     default:
